b24da438c68cebd4d03df1dfae31b6f4a1b885fd
Dan Cox <dan.cox@videlais.com>
2020-12-23 13:26:20 -0500

    Build upload

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -15,7 +15,7 @@ module.exports = {
     ],
   },
   output: {
-    path: path.resolve(__dirname, './dist'),
+    path: path.resolve(__dirname, './build'),
     filename: 'simpleqbn.bundle.js',
   }
 };
